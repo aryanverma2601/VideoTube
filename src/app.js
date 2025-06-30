f3c553d33b7999d32dfeadf3a2b8c1bd583d1514
aryanverma2601 <aryanv380@gmail.com>
2025-06-30 14:04:29 +0530

    Adding Files

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express, { text, urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //data from URL
+app.use(express.static("public"));
+app.use(cookieParser()); //to perform CRUD operations on users COOKIES
+
+export { app };
---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require("dotenv").config({ path: "./env" }); to use import type
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
+
+/*
+import express from "express";
+const app = express();
+//whenever connecting to database always use try and catch
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening at ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("error", error);
+    throw err;
+  }
+})();
+*/
